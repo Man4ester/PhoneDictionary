0b1682c537f30c2099c16b2a88d125235e5ebb2b
Man4ester <byrkovskiy@gmail.com>
2015-06-07 15:19:34 +0300

    #36 javaDoc

--- phone/src/main/java/com/hmel/Config.java
+++ phone/src/main/java/com/hmel/Config.java
@@ -3,6 +3,9 @@ package com.hmel;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
+/**
+ * @author Burkovskiy Alexander
+ */
 @Configuration
 @ImportResource("classpath:spring-context.xml")
 public class Config {
